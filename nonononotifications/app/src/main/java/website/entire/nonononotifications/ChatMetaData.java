3c0c08b640b81322dd63ce31f728639d5ce43c93
Kyle <kylemsguy@users.noreply.github.com>
2020-05-17 00:51:49 -0700

    Implement open FB messenger thread on notification tap

---
+++ nonononotifications/app/src/main/java/website/entire/nonononotifications/ChatMetaData.java
@@ -0,0 +1,10 @@
+package website.entire.nonononotifications;
+
+public class ChatMetaData {
+    public String uid;
+    public long timestamp;
+    public String first_name;
+    ChatMetaData() {
+
+    }
+}
