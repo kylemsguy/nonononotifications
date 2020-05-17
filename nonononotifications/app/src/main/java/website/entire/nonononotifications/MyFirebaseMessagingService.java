dd856a0ec14946bc8d20d15f29412b55e1f37b10
Kyle <kylemsguy@users.noreply.github.com>
2020-05-16 17:29:29 -0700

    Change to use custom notification

--- nonononotifications/app/src/main/java/website/entire/nonononotifications/MyFirebaseMessagingService.java
+++ nonononotifications/app/src/main/java/website/entire/nonononotifications/MyFirebaseMessagingService.java
@@ -99,6 +99,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.getNotification().getBody());
     }
     // [END receive_message]
 
